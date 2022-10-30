e62074e4c1e45326f172bbfee36445a632721c5c
Hamim Reza <76872754+hrshammo@users.noreply.github.com>
2022-10-31 01:15:08 +0600

    v1.0.1

--- index.php
+++ index.php
@@ -97,12 +97,7 @@
     <p>Profile</p>
    </a>
   </div>
-  <div class="dropdown headerimg">
-    <a href="#">
-    <img src="img/wishlist.png" alt="" srcset="">
-    <p>Wishlist</p>
-   </a>
-  </div>
+ 
   <div class="dropdown headerimg">
     <a href="#">
     <img src="img/cart.png" alt="" srcset="">
@@ -136,23 +131,39 @@
 <div class="catag reveal">
   <div class="c_img_bg">
     <img src="img/men.png" alt="">
+    <h3>Men</h3>
+   
   </div>
   <div class="c_img_bg">
     <img src="img/woman.png" alt="">
+    <h3>Woman</h3>
   </div>
   <div class="c_img_bg">
     <img class="bag_img" src="img/bag.png" alt="">
+    <h3>Bagpack</h3>  
   </div>
   <div class="c_img_bg">
     <img src="img/shoes.png" alt="">
+    <h3>shoes</h3>
+  </div>
+  <div class="c_img_bg ">
+    <img  src="img/asscories.png" alt="">
+    <h3>Asscories</h3>
   </div>
   <div class="c_img_bg">
     <img src="img/watch.png" alt="">
+    <h3>Wareable</h3>
   </div>
   <div class="c_img_bg">
     <img src="img/sunglass1.png" alt="">
+    <h3>Sunglass</h3>
   </div>
-
+  <div class="c_img_bg ">
+    <img class="fitness" src="img/fitness1.png" alt="">
+    <h3>Fitness</h3>
+  </div>
+  
+ 
 </div>
 
 
@@ -170,6 +181,35 @@
 
 
 
+<footer>
+  <div class="footermain">
+    <div class="app">
+        <h3>EXPERIENCE HRX APP ON MOBILE</h3>
+        <a href="#" >
+            <img src="img/google_play.png" alt="" srcset="">
+        </a><br>
+        <a href="#"> 
+            <img src="img/apple_store.png" alt="" srcset="">
+        </a>
+    </div>
+    <div class="aboutus">
+      <h3>ABOUT US</h3>
+      <p>
+        HRX is a major Bangladeshi fashion e-commerce company headquartered in Dhaka, Bangladesh. The company was founded in 2012 to sell personalized gift items. In May 2014, HRX began selling fashion and lifestyle products and moved away from personalisation. By 2018 HRX offered products from 350 Bangladeshi and International brands. 
+      </p>
+  </div>
+    <div class="social">
+            <h3>KEEP IN TOUCH
+            </h3>
+        <ul>
+            <li><a href="#"><img src="img/fb.png" alt="" srcset=""></a></li>
+            <li><a href="#"><img src="img/insta.png" alt="" srcset=""></a></li>
+            <li><a href="#"><img src="img/twitter.png" alt="" srcset=""></a></li>
+            <li><a href="#"><img src="img/link.png" alt="" srcset=""></a></li>
+       </ul>
+    </div>
 
+    </div>
+  </footer>
  </body>
  </html>
